7ebd8031c8d11dbcd92d9a8099f7e826fd5d322c
danylobyelikov-hash <danylobyelikov@gmail.com>
2026-02-21 22:36:30 -0500

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-window.FIREBASE_CONFIG = {
-  apiKey: "AIzaSyDTJa09bCiGXf48Wey1vJ549BRq3QBnvAM",
-  authDomain: "collaborate3-90c76.firebaseapp.com",
-  projectId: "collaborate3-90c76",
-  storageBucket: "collaborate3-90c76.firebasestorage.app",
-  messagingSenderId: "598125361080",
-  appId: "1:598125361080:web:0c550f5db309c3d7f3c14d"
-};
